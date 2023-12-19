cbcb9a51e86b2fce5d37ad4381f8d7a6a61b3f46
ay4748 <ay4748@bs.amalnet.k12.il>
2023-12-19 09:36:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textview3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.textview3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView tV;
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+
+    }
+
+    public void dodge(View view) {
+        counter +=1;
+        tV.setText("This is a click number:"+counter);
+        if (counter==7)
+        {
+            tV.setText("Enough to click. Go to new start!");
+        }
+    }
+}
